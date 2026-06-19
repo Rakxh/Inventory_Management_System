3ebf368fa2440ba7ff48745c082f9d52a71c40e0
Rakesh Kumar Jena <121413260+Rakxh@users.noreply.github.com>
2026-06-19 14:01:19 +0530

    Delete frontend directory

--- frontend/src/test/setup.js
+++
@@ -1,4 +0,0 @@
-import "@testing-library/jest-dom/vitest";
-
-// jsdom doesn't implement window.__APP_CONFIG__ (normally injected by config.js)
-window.__APP_CONFIG__ = { API_URL: "http://localhost:8000" };
